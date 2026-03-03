9526c1f5f2d836163dc39f944b9e28c50029d634
GSTkilla9 <Drew_Borneman@student.davincischools.org>
2026-03-03 11:54:49 -0800

    Update SonicPiCode.rb

--- SonicPiCode.rb
+++ SonicPiCode.rb
@@ -57,3 +57,76 @@ live_loop :strong do
   sample :ambi_choir, amp: 5
   sleep 8
 end
+
+
+
+
+
+
+
+
+
+use_bpm 131
+live_loop :drew do
+  use_synth :organ_tonewheel
+  play :e5, amp: 3
+  sleep 0.5
+  play :e4, amp: 3
+  sleep 0.5
+  play :b4, amp: 3
+  sleep 0.5
+  play :e4, amp: 3
+  sleep 0.5
+  play :f5, amp: 3
+  sleep 0.5
+  play :e4, amp: 3
+  sleep 0.5
+  play :b4, amp: 3
+  sleep 0.5
+  play :e4, amp: 3
+  sleep 0.5
+  play :g5, amp: 3
+  sleep 0.5
+  play :e4, amp: 3
+  sleep 0.5
+  play :b4, amp: 3
+  sleep 0.5
+  play :e4, amp: 3
+  sleep 0.5
+  play :b4, amp: 3
+  sleep 0.5
+  play :e4, amp: 3
+  sleep 0.5
+  play :b4, amp: 3
+  sleep 0.5
+  play :e4, amp: 3
+  sleep 0.5
+end
+
+sleep 16
+
+live_loop :cymbal do
+  sample :bass_trance_c, amp: 2
+  sleep 4
+end
+
+sleep 16
+
+live_loop :bass do
+  sample :drum_bass_hard, amp: 3
+  sleep 2
+end
+
+sleep 16
+
+live_loop :dub do
+  sample :ambi_choir, amp: 3, rate: -1
+  sleep 8
+end
+
+sleep 16
+
+live_loop :hey do
+  sample "C:/Users/Drew_Borneman/Documents/Audacity/11 SOUND EFFECTS (High Quality) - RBTrapHipHopRapRemixDubstep - Popular in 2020.wav", amp: 4
+  sleep 1.25
+end
